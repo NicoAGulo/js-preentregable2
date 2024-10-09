6b24cfb9973408cdf8221df97c07c4bd9eccaed2
Nicolás Alberto Gulo <nicogulo@gmail.com>
2024-10-09 18:17:21 -0300

    Se agrego contenido para simular una calculadora

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,43 @@
-import { Tempo } from 'tempo';
+//import { Tempo } from 'tempo';
+
+const btnAgregar = document.getElementById('btn-producto');
+const inputProducto = document.getElementById('producto');
+const inputValor = document.getElementById('valor');
+const carrito = document.getElementById('carrito');
+const totalCarrito = document.getElementById('total-carrito');
+let productos = [];
+
+btnAgregar.addEventListener('click', () => {
+    const producto = inputProducto.value;
+    const valor = inputValor.value;
+
+    // Crear un objeto para representar el producto y su valor
+    const nuevoProducto = {
+        producto: producto,
+        valor: valor
+    };
+
+    // Agregar el nuevo producto al arreglo
+    productos.push(nuevoProducto);
+
+    // Limpiar los inputs
+    inputProducto.value = '';
+    inputValor.value = '';
+
+    // Crear elementos de lista y agregarlos al carrito
+    let listaHTML = '';
+    productos.forEach(producto => {
+        listaHTML += `<li>${producto.producto} - $${producto.valor}</li>`;
+    });
+    carrito.innerHTML = listaHTML;
+
+    function calcularTotal() {
+        let total = 0;
+        productos.forEach(producto => {
+            total += parseFloat(producto.valor);
+        });
+        totalCarrito.textContent = `Total: $${total.toFixed(2)}`;
+    }
+
+    calcularTotal()
+});
