1d6f80dbae20e2d7a53198b7c585cd2f629504b4
Nicolás Alberto Gulo <nicogulo@gmail.com>
2024-11-03 22:07:44 -0300

    Se agregan inputs para mayor detalle en la informacion necesaria para tomar un pedido

--- JS/script.js
+++ JS/script.js
@@ -5,22 +5,24 @@
 //Inicializacion de variables Globales
 const btnAgregar = document.getElementById('btn-agregar-producto');
 const btnEnviar = document.getElementById('btn-enviar');
+
 const inputProducto = document.getElementById('producto');
 const inputValor = document.getElementById('valor');
+const inputNombre = document.getElementById('nombre-cliente')
+const inputTime = document.getElementById('time')
+const inputComentarios = document.getElementById('comentarios-cliente')
+
 const carrito = document.getElementById('carrito');
 const totalCarrito = document.getElementById('total-carrito');
 const cocina = document.getElementById('lista-cocina');
+var codigoDePedido = 0;
 
+
+//Inicializacion de arrays Globales
 let productosEnCarrito = [];
-
-
-
-
-
-
-
-
-
+let pedido = [];
+let listaPedidosEnCocina = []
+let listaPedidosDespachados = []
 
 
 //Declaracion de objeto de tipo Producto con funcion constructora.
@@ -43,6 +45,21 @@ class Producto {
     }
 }
 
+//Declaracion de objeto de tipo Pedido con funcion constructora.
+class Pedido {
+    static contadorPedidos = 0;
+
+    constructor(nombreCliente, horarioDespachoPedido, arrayDeProductos, valorTotal, comentariosDelPedido) {
+
+        this.codigoDePedido = Pedido.contadorPedidos++;
+        this.nombreCliente = nombreCliente;
+        this.horarioDespachoPedido = horarioDespachoPedido;
+        this.arrayDeProductos = arrayDeProductos;
+        this.valorTotal = valorTotal;
+        this.comentariosDelPedido = comentariosDelPedido;
+    }
+}
+
 //Suma de los valores de productos ingresados a "productosEnCarrito".
 function calcularTotal() {
     console.log('Se ejecuto un calcularTotal()')
@@ -54,24 +71,31 @@ function calcularTotal() {
     console.log(`El total es ${totalCarrito.textContent}`);
 }
 
-//Borra contenido HTML del elemento de id "carrito".
+//Borra contenido HTML para no superponer valores antigüos con los nuevos.
 function limpiarCarritoHTML() {
-    // console.log('Se procedera a limpiar el contenido HTML del carrito')
     while (carrito.firstChild) {
         carrito.removeChild(carrito.firstChild);
     }
-    // console.log('Se limpio carrito')
 }
 
-//Imprime en consola todos los items del array.
+//Imprime en consola todos los items del array. (Funcion para inspecionar).
 function recorrerContenidoDeArray(arrayARecorrer) {
     arrayARecorrer.forEach((item) => {
         console.log(`Item: ${item}`);
     });
 }
 
-//Limpia Inputs
-function limpiarInputs() {
+//Limpiar Inputs de pedido
+function limpiarInputsPedido() {
+    inputProducto.value = '';
+    inputValor.value = '';
+    inputNombre.value = '';
+    inputTime.value = '';
+    inputComentarios.value = '';
+}
+
+//Limpiar Inputs de producto
+function limpiarInputsProducto() {
     inputProducto.value = '';
     inputValor.value = '';
     // console.log('Inputs limpios');
@@ -86,12 +110,19 @@ function limpiarCarritoDeProductos() {
     console.log('El array "productosEnCarrito[]" se reseteo correctamente');
 }
 
+//Limpiar todos los pedidos de cocina.
+function limpiarPedidosDeCocina() {
+    while (cocina.firstChild) {
+        cocina.removeChild(cocina.firstChild);
+    }
+}
+
 //Limpia todo el sector de recepcion.
 function limpiarRecepcion() {
-    console.log('Se limpia HTML, Inputs y Array. Ademas se lleva a 0 la cuenta.');
+    console.log('Se limpia HTML, Inputs de y Array. Ademas se lleva a 0 la cuenta.');
 
     limpiarCarritoHTML();
-    limpiarInputs();
+    limpiarInputsPedido();
     limpiarCarritoDeProductos();
     calcularTotal();
 }
@@ -133,33 +164,56 @@ function agregaItemAlPedido(elemento, indice) {
     calcularTotal();
 }
 
+
+//Funcion para agregar el pedido al listado HTML
+function agregarPedidoACocina(pedido) {
+
+    const item = document.createElement('li');
+    item.innerHTML = `
+    Pedido N°: ${pedido.codigoDePedido} 
+    Nombre: ${pedido.nombreCliente}
+    Hora: ${pedido.horarioDespachoPedido}
+    ${pedido.valorTotal}
+    Comentarios: ${pedido.comentariosDelPedido}
+    `;
+
+    pedido.arrayDeProductos.forEach((producto) => {
+        const itemDelProducto = document.createElement('li');
+        itemDelProducto.innerHTML = `Codigo: ${producto.codigo}   Nombre: ${producto.nombre}   Valor: $${producto.valor}`;
+
+        item.appendChild(itemDelProducto);
+    })
+
+    cocina.appendChild(item);
+    limpiarRecepcion();
+}
+
 //Actualiza el listado de items en el pedido (ejecuta agregaItemAlPedido)
 function actualizarPedido() {
-    productosEnCarrito.forEach((producto, indice) => {
-        agregaItemAlPedido(producto, indice);
+    productosEnCarrito.forEach((producto) => {
+        agregaItemAlPedido(producto);
     });
 
 }
 
 
-
-
-
 //Se ejecuta el siguiente codigo cuando se desea ingresar un nuevo producto al carrito
 btnAgregar.addEventListener('click', () => {
-    console.log('Se agregara un nuevo producto al pedido');
-    const producto = inputProducto.value;
-    const valor = inputValor.value;
 
     //Crear un producto solo si los datos son suficientes para su creacion
     if (producto !== '' && valor !== '') {
+
+        console.log('Se agregara un nuevo producto al pedido');
+        const producto = inputProducto.value;
+        const valor = inputValor.value;
 
         // Crea un nuevo objeto para representar el producto y el valor ingresado
         const nuevoProducto = new Producto(producto, valor);
 
         // El producto nuevo es ingresado al array llamado "productosEnCarrito"
         productosEnCarrito.push(nuevoProducto);
-        limpiarInputs();
+
+        limpiarInputsProducto();
         limpiarCarritoHTML();
         actualizarPedido();
         calcularTotal();
@@ -168,21 +222,32 @@ btnAgregar.addEventListener('click', () => {
 });
 
 
-
-
-
-
 //2da parte -> Llevar un pedido con el boton de ENVIAR PEDIDO a una nueva lista englobado en un div para que se despache con otro boton de la pestaña COCINA
 btnEnviar.addEventListener('click', () => {
-    const nuevoPedido = document.getElementById('carrito');
+    if (productosEnCarrito.length !== 0) {
+        console.log('Se agrega un nuevo pedido a cocina')
 
-    let contenedorDePedidoACocinar = document.createElement('div');
+        //Se guarda en variables los datos de la compra y el cliente
+        const cliente = inputNombre.value;
+        const hora = inputTime.value;
+        const compra = productosEnCarrito;
+        const totalCompra = totalCarrito.innerHTML;
+        const comentarios = inputComentarios.value;
 
-    contenedorDePedidoACocinar.innerHTML = nuevoPedido.innerHTML;
 
-    contenedorDePedidoACocinar.innerHTML += `<button>PEDIDO FINALIZADO</button>`;
+        //crear instancia de objeto de tipo Pedido para agregar al listado de pedidos en Cocina
+        const nuevoPedido = new Pedido(cliente, hora, compra, totalCompra, comentarios);
 
-    cocina.appendChild(contenedorDePedidoACocinar);
+        //Agrega el nuevo pedido a la lista
+        listaPedidosEnCocina.push(nuevoPedido);
 
-    limpiarRecepcion();
+        //Agrega el nuevo pedido a la lista HTML
+        agregarPedidoACocina(nuevoPedido);
+
+        //Se limpia el contenido del carrito para agregar el proximo pedido con productos
+        limpiarCarritoDeProductos();
+
+        //Se limpia inputs para habilitar la nueva toma de pedidos
+        limpiarRecepcion();
+    }
 });
